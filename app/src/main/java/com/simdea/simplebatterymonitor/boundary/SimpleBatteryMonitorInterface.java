7f5939080f8b12bbda22abf1028a3e15b04b30f8
paulo-ribeiro-bold <paulo.ribeiro@muzzley.com>
2016-02-22 15:20:28 +0000

    Addition of the Boundary (Interface Layer) and System (System Layer) packages.

---
+++ app/src/main/java/com/simdea/simplebatterymonitor/boundary/SimpleBatteryMonitorInterface.java
@@ -0,0 +1,7 @@
+package com.simdea.simplebatterymonitor.boundary;
+
+/**
+ * Created by Paulo on 2/22/2016.
+ */
+public interface SimpleBatteryMonitorInterface {
+}
---
+++ app/src/main/java/com/simdea/simplebatterymonitor/system/BatteryMonitor.java
@@ -0,0 +1,8 @@
+package com.simdea.simplebatterymonitor.system;
+
+/**
+ * Created by Paulo on 2/22/2016.
+ */
+public class BatteryMonitor {
+
+}
